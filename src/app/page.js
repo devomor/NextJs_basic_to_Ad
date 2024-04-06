8f1a3e9675e59306e4d7e8b7d6934f42e90aff29
softdevomar <omorfarukqqq@gmail.com>
2024-04-06 03:45:24 -0700

    alert method call

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
-
+"use client"
 export default function Home() {
+
+  const fun= ()=>{
+    alert("hellow programmer")
+  }
   return (
     <main >
     <h1>hellow programmer</h1>
+    <button onClick={fun}>click me</button>
     </main>
   );
 }
