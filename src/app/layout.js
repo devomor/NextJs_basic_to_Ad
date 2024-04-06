0c6c64e9ae2c7a9dfdd3be58f487dd62790895e3
softdevomar <omorfarukqqq@gmail.com>
2024-04-06 10:34:56 -0700

    componet call layout navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../component/navbar/Navbar.jsx"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <header style={{backgroundColor:'lightblue',padding:'1rem'}}>
-        <p>header</p>
-      </header>
+        <Navbar />
       {children}
       <footer style={{backgroundColor:'lightblue',padding:'1rem'}}>
         <p>footer</p>
---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div>
+      this is a navbar
+    </div>
+  )
+}
